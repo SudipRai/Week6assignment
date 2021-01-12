d9c69916b0c5d2ed2a599ca7a2ba5f4a1af84d21
Sudip Rai <raisudip05@gmail.com>
2021-01-12 15:56:45 +0545

    2nd commit

--- app/src/main/java/com/sudip/week6assignment/Fragments/AddFragment.kt
+++ app/src/main/java/com/sudip/week6assignment/Fragments/AddFragment.kt
@@ -5,10 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import com.sudip.week6assignment.R
 
 
 class AddFragment : Fragment() {
+
+    private lateinit var edtname: EditText
+    private lateinit var edtage: EditText
+    private lateinit var edtaddress: EditText
+    private lateinit var rg: RadioGroup
+    private lateinit var rdomale: RadioButton
+    private lateinit var rdofemale: RadioButton
+    private lateinit var rdoother: RadioButton
+    private lateinit var btnsave:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,7 +32,16 @@ class AddFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_add, container, false)
+        edtname = view.findViewById(R.id.edtname)
+        edtage = view.findViewById(R.id.edtage)
+        edtaddress = view.findViewById(R.id.edtaddress)
+        rg=view.findViewById(R.id.rg)
+        rdomale=view.findViewById(R.id.rdomale)
+        rdofemale=view.findViewById(R.id.rdofemale)
+        rdoother=view.findViewById(R.id.rdoother)
+        btnsave=view.findViewById(R.id.btnsave)
 
+        return view
+    }
 }
